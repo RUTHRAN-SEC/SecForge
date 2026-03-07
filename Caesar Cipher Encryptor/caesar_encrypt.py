3055aa33b987f625f9c1333c16ef6b2126b65473
RUTHRAN <ruthran.sec@gmail.com>
2026-03-07 12:41:48 +0530

    Update caesar_encrypt.py

--- Caesar Cipher Encryptor/caesar_encrypt.py
+++ Caesar Cipher Encryptor/caesar_encrypt.py
@@ -1,6 +1,6 @@
-def decrypt_caesar(ciphertext, shift):
-    # Create empty string to store decrypted result
-    decrypted = ""
+def encrypt_caesar(ciphertext, shift):
+    # Create empty string to store encrypt result
+    encrypted = ""
 
     # Loop through each character in the input text
     for char in ciphertext:
@@ -16,31 +16,31 @@ def decrypt_caesar(ciphertext, shift):
             
             # Convert letter to number using ord()
             # Subtract base ASCII value
-            # Subtract shift to decrypt
+            # Subtract shift to encrypt
             # Use % 26 to wrap around alphabet
             # Convert back to character using chr()
-            decrypted += chr((ord(char) - ascii_offset - shift) % 26 + ascii_offset)
+            encrypted += chr((ord(char) - ascii_offset - shift) % 26 + ascii_offset)
         
         else:
             # If character is not a letter (space, number, symbol)
             # Just add it as it is
-            decrypted += char
+            encrypted += char
 
-    # Return the final decrypted text
-    return decrypted
+    # Return the final encrypted text
+    return encrypted
 
 
 if __name__ == "__main__":
     
     # Ask user to enter encrypted message
-    ciphertext = input("Enter the text to decrypt: ").strip()
+    ciphertext = input("Enter the text to encrypt: ").strip()
     
     # Ask user to enter shift value
     # Convert input string into integer
     shift = int(input("Enter the shift you want: ").strip())
 
     # Call decrypt function and store result
-    result = decrypt_caesar(ciphertext, shift)
+    result = encrypt_caesar(ciphertext, shift)
 
     # Print decrypted result
     print(result)
